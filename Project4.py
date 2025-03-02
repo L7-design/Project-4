5b77bc3bf983abdf77dced254d374e020a46a17e
L7-design <198395689+L7-design@users.noreply.github.com>
2025-03-02 12:10:33 -0800

    Update Project4.py

--- Project4.py
+++ Project4.py
@@ -1,1 +1,11 @@
+#import libraries 
+import pandas as pd
+import streamlit as st
+import plotly.express as px 
+import altair as alt
+
+#Load dataset into the root directory 
+data = pd.read_csv('/datasets/vehicles_us.csv')
+
+#Create an EDA.ipynb Jupyter notebook 
 
