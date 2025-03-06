a2fdc49bcc4c63a466d8ed90b71b110907ed14cd
L7-design <198395689+L7-design@users.noreply.github.com>
2025-03-05 20:33:34 -0800

    first submit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Project-4
+
+#Description: 
+This project aims to perform an analysis for a used car company, examining the dataset for patterns in car valuations and factors that impact the amount of time it takes to sell a used car.
+
+#SetUp 
+1. Ensure all required accounts are created and appropriate packages installed.
+2. Download the appropriate data file.
+3. Explore the data and analyze the data.
+4. Set up a web application dashboard
+5. Deploy the web appliction using Render.  
--- app.py
+++ app.py
@@ -1,8 +1,60 @@
+#import libraries 
 import streamlit as st 
 import pandas as pd
-
-
-st.header('Title')
+import plotly.express as px
+import matplotlib.pyplot as plt
 
 #Load dataset
 data = pd.read_csv('vehicles_us.csv')
+
+#Cleaning data 
+#Correcting datatypes for model_year: 
+data['model_year']=data['model_year'].astype('Int64')
+
+#Correcting datatypes for odometer: 
+data['odometer']=data['odometer'].astype('Int64')
+
+#Correcting datatypes for date_posted: 
+data['date_posted'] = pd.to_datetime(data['date_posted'],format ='%Y-%m-%d')
+
+#Filling missing values for is_4wd
+data['is_4wd']=data['is_4wd'].fillna(0)
+
+#Filling missing values for odometer
+data['odometer']=data['odometer'].fillna(0)
+
+#Filling missing values for paint_color
+data['paint_color']=data['paint_color'].fillna('unkown')
+
+#Filling missing values for cylinders
+data['cylinders']=data['cylinders'].fillna('unkown')
+
+#Dropping missing values for model_year 
+data = data.dropna(subset=['model_year'])
+
+st.header('Visualizations of the Data for Price & Type of Car')
+
+#Creating a Plotly Express Histogram 
+fig1 = px.histogram(data[data['price']<=100000], x= 'price',
+title='Price Distribution')
+st.plotly_chart(fig1)
+
+
+
+#Creating a Plotly Express Scatter Plot 
+fig2 = px.scatter(data, x='type', 
+y= 'price', title= 'Price Compared to Type')
+st.plotly_chart(fig2)
+
+#Creating a Checkbox  
+show_plot=st.checkbox("Show Scatter Plot") 
+
+if show_plot: 
+    fig3,ax = plt.subplots() 
+    ax.scatter(data['odometer'],data['price']) 
+    ax.set_xlabel('Odometer')
+    ax.set_ylabel('Price') 
+    ax.set_title('Odometer Compared to Price') 
+    st.pyplot(fig3) 
+else:
+    st.write("Check the box to display scatter plot.") 
